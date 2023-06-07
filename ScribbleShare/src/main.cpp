e0aacd8b80242aa35b019bf0e2eaa866adc56178
Hun2yr <hunter.coker.2016@gmail.com>
2023-06-06 22:38:13 -0400

    set up glfw with glad

--- ScribbleShare/src/main.cpp
+++ ScribbleShare/src/main.cpp
@@ -1,10 +1,94 @@
-
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 
+uint32_t width = 900;
+uint32_t height = 600;
+
+const char* vertex_shader_source =
+	"#version 330 core\n"
+	"layout(location = 0) in vec3 aPos;\n"
+	"void main() {\n"
+	"	gl_Position = vec4(aPos, 1.0);\n"
+	"}\0";
+
+const char* fragment_shader_source =
+	"#version 330 core\n"
+	"out vec4 fragColor;\n"
+	"void main() {\n"
+	"	fragColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
+	"}\0";
+
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	/* windowing & opengl setup */
+	glfwInit();
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	GLFWwindow* window = glfwCreateWindow(width, height, "ScribbleShare", nullptr, nullptr);
+	if (!window) {
+		std::cerr << "Failed to create GLFW window!\n";
+		exit(1);
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to initialize GLAD!\n";
+		exit(1);
+	}
+
+	/* generate shader program */
+	GLuint vertex_shader, fragment_shader, shader_program;
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
+	glCompileShader(vertex_shader);
+
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
+	glCompileShader(fragment_shader);
+
+	shader_program = glCreateProgram();
+	glAttachShader(shader_program, vertex_shader);
+	glAttachShader(shader_program, fragment_shader);
+	glLinkProgram(shader_program);
+	glUseProgram(shader_program);
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+
+	/* generate texture to render pixel buffer */
+
+	/* generate & fill buffers */
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	uint32_t* canvas_buffer = new uint32_t[width * height];
+
+	GLuint vao, vbo;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+	}
 
 	return 0;
 }
